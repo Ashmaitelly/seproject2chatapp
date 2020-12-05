495b591ecc00015092bdffb925c3ed1e7744dde9
Abd Sh <email>
2020-12-05 15:30:04 +0200

    better formatted

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
-
 print('Server running...')
 
 clients = []
+
+
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
-       for client in clients:
-          if client != self:
-             client.sendMessage(self.data)
+        for client in clients:
+            if client != self:
+                client.sendMessage(self.data)
 
     def handleConnected(self):
-       print(self.address, 'connected')
-       clients.append(self)
+        print(self.address, 'connected')
+        clients.append(self)
 
     def handleClose(self):
-       clients.remove(self)
-       print(self.address, 'closed')
+        clients.remove(self)
+        print(self.address, 'closed')
+
 
 server = SimpleWebSocketServer('localhost', 8000, SimpleChat)
 server.serveforever()
